664b114e227e4fc2ca725f8e4d79ccc2e771ffa8
Alex Ames <aames@wisc.edu>
2017-12-20 13:50:24 -0600

    attempt at interpolation indexing fix

--- scratch/repl.jl
+++ scratch/repl.jl
@@ -1,6 +1,6 @@
-using FileIO, Glob
+using FileIO, Glob, ImageView
 rawdir = normpath((@__DIR__)*"/../raw/piv1B/")
-rawpaths = glob("B001*tif", rawdir)
+rawpaths = glob("B002*tif", rawdir)
 I1, I2 = float.([load(f) for f in rawpaths])
 
-folki(I1, I2; J = 1, N = 2)
+folki(I1, I2; J = 5, N = 10)
--- src/folki-cpu.jl
+++ src/folki-cpu.jl
@@ -1,12 +1,13 @@
 module FOLKI
 using Images, ImageFiltering, Interpolations
 
+medianwindow(img,n) = mapwindow(median!, img, (n,n))
 """
     u, v = folki(args...)
 
 Compute the displacement field of a pair of input images I1 and I2
 """
-function folki(I1raw, I2raw; J=3, N=10, mask=trues(I1raw), w=Kernel.gaussian(2))
+function folki(I1raw, I2raw; J=3, N=10, mask=trues(I1raw), w=Kernel.gaussian(4))
     # Input verification
     if size(I1raw) != size(I2raw)
         error("""Input images must have identical dimensions.
@@ -18,7 +19,6 @@ function folki(I1raw, I2raw; J=3, N=10, mask=trues(I1raw), w=Kernel.gaussian(2))
     P1, P2 = Images.gaussian_pyramid.([I1, I2], J-1, 2, 1.0)
     # TODO: fix inefficient alloc of u, v
     U, V = Images.gaussian_pyramid.([zeros(I1), zeros(I2)], J-1, 2, 1.0) #
-    #X, Y = copy(U), copy(V)
 
     for j in J:-1:1
         i1, i2 = P1[j], P2[j]
@@ -26,20 +26,23 @@ function folki(I1raw, I2raw; J=3, N=10, mask=trues(I1raw), w=Kernel.gaussian(2))
         np, nq = size(i1)
         iy, ix = imgradients(i1, KernelFactors.ando3)
         itp = interpolate(i2, BSpline(Quadratic(Flat())), OnGrid())
-        a, b, c = imfilter(ix.*ix, w), imfilter(iy.*iy, w), imfilter(ix.*iy, w)
-        d = @. a*b - c*c
+        ixx, iyy, ixy = imfilter(ix.*ix, w), imfilter(iy.*iy, w), imfilter(ix.*iy, w)
+        d = @. ixx*iyy - ixy^2
 
         for n = 1:N
+            @show extrema(u), extrema(v)
             for q = 1:nq, p = 1:np
                 i2[p,q] = itp[p + v[p,q], q + u[p,q]]
             end
             didt = @. u*ix + v*iy + i1 - i2
-            g, h = imfilter(ix.*didt, w), imfilter(iy.*didt, w)
-            u = (b.*g - c.*h)./d
-            v = (a.*h - c.*g)./d
-            invalid = !(isfinite.(u) & isfinite.(v))
+            ixt, iyt = imfilter(ix.*didt, w), imfilter(iy.*didt, w)
+            @. u = (iyy*ixt - ixy*iyt)/d
+            @. v = (ixx*iyt - ixy*ixt)/d
+            invalid = .!(isfinite.(u) .& isfinite.(v))
             u[invalid] = zero(u[1])
             v[invalid] = zero(v[1])
+            u .= medianwindow(u,3)
+            v .= medianwindow(v,3)
         end
 
         if j > 1
