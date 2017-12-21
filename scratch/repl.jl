9e8284acd1fe968e0d9bea77463662ed5c6d594f
Alex Ames <aames@wisc.edu>
2017-12-21 11:40:27 -0600

    bibliography, report-writing

--- scratch/repl.jl
+++ scratch/repl.jl
@@ -1,12 +1,12 @@
-using FileIO, Glob, ImageView, Plots
-using ProfileView
+using FileIO, Glob, ImageView
+import Plots: plot, pdf
 
 rawdir = normpath((@__DIR__)*"/../raw/piv1B/")
 rawpaths = glob("B002*tif", rawdir)
 I1, I2 = float.([load(f) for f in rawpaths])
 
-U, V, err = folki(I1, I2; J = 3, N = 10)
+U, V, err = folki(I1, I2; J = 3, N = 10, w = Kernel.gaussian(3))
 
 plot(err)
-imshow(U[1])
+imshow(U[3])
 imshow(sqrt(U[1].^2 + V[1].^2))
