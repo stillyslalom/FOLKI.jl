3f27fe5e6d596308ed82576ce224269a71b74e47
Alex Ames <aames@wisc.edu>
2017-12-20 22:02:22 -0600

    more rigorous filtering; started report

--- scratch/repl.jl
+++ scratch/repl.jl
@@ -1,7 +1,12 @@
-using FileIO, Glob, ImageView
-rawdir = normpath((@__DIR__)*"/../raw/piv1B/")
-rawpaths = glob("B001*tif", rawdir)
-I1, I2 = float.([load(f) for f in rawpaths])
-U, V = folki(I1, I2; J = 2, N = 10)
+using FileIO, Glob, ImageView, Plots
+using ProfileView
 
+rawdir = normpath((@__DIR__)*"/../raw/piv1B/")
+rawpaths = glob("B002*tif", rawdir)
+I1, I2 = float.([load(f) for f in rawpaths])
+
+U, V, err = folki(I1, I2; J = 3, N = 10)
+
+plot(err)
+imshow(U[1])
 imshow(sqrt(U[1].^2 + V[1].^2))
